240b94e64f5c22cef13a9a5cee200e70510346b0
Eugene Cheipesh <echeipesh@gmail.com>
2019-04-18 17:44:07 -0700

    Keep only job skeleton

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -25,12 +25,10 @@ object Main extends CommandApp(
   header = "Creates a Spark batch job",
   main = {
     val inputsOpt = Opts.options[String]("inputPath", help = "The path that points to data that will be read")
-    val nameOpt = Opts.option[String]("name", help = "The name of the output layer")
-    val zoomOpt = Opts.option[Int]("zoom", help = "The max zoom level the catalog should be saved as").withDefault(13)
-    val numPartitionsOpt = Opts.option[Int]("numPartitions", help = "The number of partitions to use").orNone
     val outputOpt = Opts.option[String]("outputPath", help = "The path of the output catlaog")
+    val numPartitionsOpt = Opts.option[Int]("numPartitions", help = "The number of partitions to use").orNone
 
-    (inputsOpt, nameOpt, zoomOpt, numPartitionsOpt, outputOpt).mapN { (inputs, name, zoom, numPartitions, output) =>
+    (inputsOpt, outputOpt, numPartitionsOpt).mapN { (inputs, output, numPartitions) =>
       val conf = new SparkConf()
         .setIfMissing("spark.master", "local[*]")
         .setAppName("GeoTrellis Batch Job")
@@ -38,45 +36,12 @@ object Main extends CommandApp(
         .set("spark.kryo.registrator", "geotrellis.spark.io.kryo.KryoRegistrator")
         .set("spark.executionEnv.AWS_PROFILE", Properties.envOrElse("AWS_PROFILE", "default"))
 
-      implicit val ss: SparkSession = SparkSession.builder.config(conf).enableHiveSupport.getOrCreate
-      implicit val sc: SparkContext = ss.sparkContext
+      implicit val spark = SparkSession.builder.config(conf).enableHiveSupport.getOrCreate
+      implicit val sc = ss.sparkContext
 
-      try {
-        val tileLayer: MultibandTileLayerRDD[SpatialKey] = ProcessInputs(inputs.toList, zoom, numPartitions)
+      // Job logic ...
 
-        tileLayer.persist()
-
-        val pyramid: Stream[(Int, MultibandTileLayerRDD[SpatialKey])] =
-          Pyramid.levelStream(tileLayer, ZoomedLayoutScheme(WebMercator), startZoom = zoom, endZoom = 0)
-
-        val layerWriter: LayerWriter[LayerId] = LayerWriter(output)
-
-        // TODO: Save a histogram for each band.
-        // Currently, giter8 has problems creatings templates that
-        // have string interpolation in its code.
-        // See this issue for more info: https://github.com/foundweekends/giter8/issues/333
-
-        /*
-        val histograms: Array[Histogram[Double]] = tileLayer.histogram
-
-        histograms.zipWithIndex.foreach { case (hist, index) =>
-          layerWriter
-            .attributeStore
-            .write[Histogram[Double]](LayerId(name, zoom), s"band\_\${index}\_histogram", hist)
-        }
-        */
-
-        pyramid.foreach { case (z, layer) =>
-          layerWriter.write(LayerId(name, z), layer, ZCurveKeyIndexMethod)
-        }
-
-        tileLayer.unpersist()
-
-      } catch {
-        case e: Exception => throw e
-      } finally {
-        sc.stop()
-      }
+      spark.stop()
     }
   }
 )
