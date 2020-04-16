c5620b79b8a27542c1c7e0e25cc05dd8b11c12c0
Eugene Cheipesh <echeipesh@gmail.com>
2020-04-16 13:53:56 -0400

    Cleanup Main

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -1,28 +1,23 @@
 package geotrellis.batch
 
+import geotrellis.layer._
+import geotrellis.vector._
+import geotrellis.proj4._
 import geotrellis.raster._
-import geotrellis.raster.histogram.Histogram
-import geotrellis.raster.io._
 import geotrellis.spark._
-import geotrellis.spark.pyramid.Pyramid
-import geotrellis.spark.tiling.ZoomedLayoutScheme
-import geotrellis.spark.io._
-import geotrellis.spark.io.index.ZCurveKeyIndexMethod
-import geotrellis.spark.io.kryo.KryoRegistrator
-import geotrellis.proj4.WebMercator
 
 import cats.implicits._
 import com.monovore.decline._
 
 import org.apache.spark._
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql._
 import org.apache.spark.rdd._
 
 import scala.util.Properties
 
 object Main extends CommandApp(
   name = "$name$",
-  header = "Creates a Spark batch job",
+  header = "GeoTrellis Spark: $name$",
   main = {
     val inputsOpt = Opts.options[String]("inputPath", help = "The path that points to data that will be read")
     val outputOpt = Opts.option[String]("outputPath", help = "The path of the output catlaog")
@@ -31,7 +26,7 @@ object Main extends CommandApp(
     (inputsOpt, outputOpt, numPartitionsOpt).mapN { (inputs, output, numPartitions) =>
       val conf = new SparkConf()
         .setIfMissing("spark.master", "local[*]")
-        .setAppName("GeoTrellis Batch Job")
+        .setAppName("$name$")
         .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
         .set("spark.kryo.registrator", "geotrellis.spark.io.kryo.KryoRegistrator")
         .set("spark.executionEnv.AWS_PROFILE", Properties.envOrElse("AWS_PROFILE", "default"))
@@ -39,9 +34,11 @@ object Main extends CommandApp(
       implicit val spark = SparkSession.builder.config(conf).enableHiveSupport.getOrCreate
       implicit val sc = spark.sparkContext
 
-      // Job logic ...
-
-      spark.stop()
+      try {
+        // Job logic ...
+      } finally {
+        spark.stop()
+      }
     }
   }
 )
