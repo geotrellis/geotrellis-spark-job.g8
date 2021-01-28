1d1ffac4c81b06124cf146c779a8dbd46dcbeea2
Eugene Cheipesh <echeipesh@gmail.com>
2021-01-28 09:38:32 -0500

    add things

--- src/main/g8/src/main/scala/$organization__packaged$/Main.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/Main.scala
@@ -13,31 +13,25 @@ import org.apache.spark._
 import org.apache.spark.sql._
 import org.apache.spark.rdd._
 
-import scala.util.Properties
+import org.log4s._
 
 object Main extends CommandApp(
   name = "$name$",
   header = "GeoTrellis Spark: $name$",
   main = {
+    @transient val logger = getLogger
+
     val inputsOpt = Opts.options[String]("inputPath", help = "The path that points to data that will be read")
     val outputOpt = Opts.option[String]("outputPath", help = "The path of the output catlaog")
     val numPartitionsOpt = Opts.option[Int]("numPartitions", help = "The number of partitions to use").orNone
 
     (inputsOpt, outputOpt, numPartitionsOpt).mapN { (inputs, output, numPartitions) =>
-      val conf = new SparkConf()
-        .setIfMissing("spark.master", "local[*]")
-        .setAppName("$name$")
-        .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-        .set("spark.kryo.registrator", "geotrellis.spark.store.kryo.KryoRegistrator")
-        .set("spark.executionEnv.AWS_PROFILE", Properties.envOrElse("AWS_PROFILE", "default"))
-
-      implicit val spark = SparkSession.builder.config(conf).enableHiveSupport.getOrCreate
-      implicit val sc = spark.sparkContext
+      implicit val sc = Spark.session.sparkContext
 
       try {
         // Job logic ...
       } finally {
-        spark.stop()
+        Spark.session.stop()
       }
     }
   }
